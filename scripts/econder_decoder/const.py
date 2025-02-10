e320dd6a829d49a825e5e3c54aafb7f64387d166
graziaperna <g.perna4@studenti.uniba.it>
2025-02-10 18:25:42 +0100

    encoder_decoder impl

---
+++ scripts/econder_decoder/const.py
@@ -0,0 +1,3 @@
+AUDIO_EMBEDDING_NAME = "model_weights/dense/sequential/dense/kernel"
+TEXT_EMBEDDING_NAME = "model_weights/embedding/embedding/embeddings"
+MOVEMENT_EMBEDDING_NAME = "model_weights/dense_2/sequential/dense_2/kernel"
